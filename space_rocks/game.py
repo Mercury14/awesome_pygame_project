866d1a02672512fe5e202bbf57a6d102a200ba2b
SimonB <bartlett.se@googlemail.com>
2021-06-18 09:36:47 +0100

    Added space background

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,9 +1,12 @@
 import pygame
 
+from utils import load_sprite
+
 class SpaceRocks:
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
+        self.background = load_sprite("space", False)
     
     def main_loop(self):
         while True:
@@ -17,12 +20,13 @@ class SpaceRocks:
     
     def _handle_input(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 quit()
 
     def _process_game_logic(self):
         pass
 
     def _draw(self):
-        self.screen.fill((0, 0, 255))
+        self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
