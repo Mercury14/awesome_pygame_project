1ffa5db26f53408b677499537d9a93b6f1b157b7
SimonB <bartlett.se@googlemail.com>
2021-07-03 16:35:46 +0100

    Added messages

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from models import Asteroid, Spaceship
-from utils import get_random_position, load_sprite
+from utils import get_random_position, load_sprite, print_text
 
 class SpaceRocks:
     MIN_ASTEROID_DISTANCE = 250
@@ -10,6 +10,8 @@ class SpaceRocks:
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("space", False)
         self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(None, 64)
+        self.message = ""
 
         self.asteroids = []
         self.bullets = []
@@ -59,6 +61,7 @@ class SpaceRocks:
             for asteroid in self.asteroids:
                 if asteroid.collides_with(self.spaceship):
                     self.spaceship = None
+                    self.message = "You lost!"
                     break
 
         for bullet in self.bullets[:]:
@@ -73,11 +76,17 @@ class SpaceRocks:
             if not self.screen.get_rect().collidepoint(bullet.position):
                 self.bullets.remove(bullet)
 
+        if not self.asteroids and self.spaceship:
+            self.message = "You won!"
+
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
         for game_object in self._get_game_objects():
             game_object.draw(self.screen)
         
+        if self.message:
+            print_text(self.screen, self.message, self.font)
+
         pygame.display.flip()
         self.clock.tick(60)
     
